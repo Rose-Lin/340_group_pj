58627a87b9718c7c7e2abfcefc032e8596d6c16c
Jiayi Lin <rlin1@brynmawr.edu>
2018-09-24 16:18:38 -0400

    parse pref list and return a dict

--- data/basic/parsetxt.py
+++ data/basic/parsetxt.py
@@ -7,16 +7,27 @@ def parse_classTimes(file):
         teachers_lines = raw_content.splitlines(True)[8:]
     for line in lines:
         tokens = line.split('\t')
-        class_time = tokens[0]
-        room = tokens[1]
+        class_time = int(tokens[0])
+        room = int(tokens[1])
         print("class_time: {}---room:{}".format(class_time, room))
     for line in teachers_lines:
         tokens = line.split('\t')
-        class_num = tokens[0]
-        teachers = tokens[1]
+        class_num = int(tokens[0])
+        teachers = int(tokens[1])
         print("class_num: {}---teachers: {}".format(class_num, teachers))
 
 def parse_pref(file):
-    pass
+    dict = {}
+    with open(file) as f:
+        raw_content = f.read()
+        lines = raw_content.splitlines(True)[1:]
+        for line in lines:
+            student_id = int(line.split('\t')[0])
+            pref_list_line = line.split('\t')[1]
+            pref_list = [int(x) for x in pref_list_line.split()]
+            dict[student_id] = pref_list
+    return dict
+
 
 parse_classTimes("./demo_constraints.txt")
+parse_pref("./demo_studentprefs.txt")
