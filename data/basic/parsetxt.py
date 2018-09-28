d527b57efc2a4ba521f352ab7b4ad74e1199dec4
Yuge Shen <Caroline@wc122-dhcp-155.brynmawr.edu>
2018-09-28 18:28:09 -0400

    calculate the edgeWeights

--- data/basic/parsetxt.py
+++ data/basic/parsetxt.py
@@ -10,12 +10,12 @@ def parse_classTimes(file):
         tokens = line.split('\t')
         class_time = int(tokens[0])
         room = int(tokens[1])
-        print("class_time: {}---room:{}".format(class_time, room))
+        # print("class_time: {}---room:{}".format(class_time, room))
     for line in teachers_lines:
         tokens = line.split('\t')
         class_num = int(tokens[0])
         teachers = int(tokens[1])
-        print("class_num: {}---teachers: {}".format(class_num, teachers))
+        # print("class_num: {}---teachers: {}".format(class_num, teachers))
 
 def parse_pref(file):
     dict = {}
@@ -40,8 +40,30 @@ def count_class_size(pref_dict):
                 sizes[index] = 1
     n = sorted(sizes.items(), key=operator.itemgetter(1))
     n.reverse()
+    # print(n)
+
+def edgeWeights(dict):
+    weight = {}
+    for student in dict.keys():
+        for i in range(len(dict[student])):
+            for j in range (i+1, len(dict[student])):
+                key1 = dict[student][i]
+                key2 =dict[student][j]
+                keyMin = min(key1,key2)
+                keyMax = max(key1, key2)
+                key = (keyMin, keyMax)
+                if not weight.get(key) == None:
+                    weight[key] += 1
+                else:
+                    weight[key] = 1
+    # print(weight)
+    n = sorted(weight.items(), key=operator.itemgetter(1))
+    n.reverse()
     print(n)
+    print(len(weight))
+    return weight
 
 parse_classTimes("./demo_constraints.txt")
-parse_pref("./demo_studentprefs.txt")
+dict = parse_pref("./demo_studentprefs.txt")
 count_class_size(parse_pref("./demo_studentprefs.txt"))
+edgeWeights(dict)
