0bb01cbcae44ef558e35d29a15bc99c724269582
Caroline Shen <yshen3@powerpuff.cs.brynmawr.edu>
2018-10-25 15:41:52 -0400

    sort room cap, test result

--- data/basic/parsetxt.py
+++ data/basic/parsetxt.py
@@ -42,6 +42,25 @@ def count_class_size(pref_dict):
     n.reverse()
     # print(n)
 
+def sort_room_cap(Class_list):
+    Class_list.sort(key = lambda x: x[1])
+    Class_list.reverse()
+    print (Class_list)
+    return Class_list
+    
+def test_result(S, Pref, Schedule, Position):
+    count = 0
+    total = 0
+    for s in S:
+        total += len(Pref[c])
+        final_pick = [0] * (len(Schedule)+1)
+        for c in Pref[s]:
+            t = Position[c+1][0]
+            if final_pick[t] == 0:
+                final_pick[t] = c
+                count += 1
+    return (count/total)
+
 def edgeWeights(dict):
     weight = {}
     for student in dict.keys():
@@ -66,4 +85,5 @@ def edgeWeights(dict):
 parse_classTimes("./demo_constraints.txt")
 dict = parse_pref("./demo_studentprefs.txt")
 count_class_size(parse_pref("./demo_studentprefs.txt"))
-edgeWeights(dict)
+sort_room_cap([(1,40),(2,50),(3,20),(4,30)])
+#edgeWeights(dict)
