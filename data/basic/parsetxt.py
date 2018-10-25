689abdb0b4986c9101322bba8d39a39b4d866ff8
Jiayi Lin <rlin1@brynmawr.edu>
2018-10-25 16:32:48 -0400

    algo modified to based on pop, tests needed

--- data/basic/parsetxt.py
+++ data/basic/parsetxt.py
@@ -9,16 +9,21 @@ def parse_classTimes(file):
         total_classes = raw_content.splitlines(True)[6]
         total = int(total_classes.split('\t')[1])
         total_rooms = int(raw_content.splitlines(True)[1].split('\t')[1])
+        total_time_slots = int(raw_content.splitlines(True)[0].split('\t')[1])
     # a list of professors indexed by the class id
     professors = [0]*total
     classes = [0]*total
+    # 1 indexing
     rooms = []
+    # 0 indexing
+    time_slots = []
     for line in lines:
         tokens = line.split('\t')
         class_time = int(tokens[0])
         room = int(tokens[1])
         room_id = int(tokens[0])
         #print("class_time: {}---room:{}".format(class_time, room))
+        time_slots.append(class_time-1)
         rooms.append((room_id, room))
     for line in teachers_lines:
         tokens = line.split('\t')
@@ -27,7 +32,7 @@ def parse_classTimes(file):
         # print("class_num: {}---teachers: {}".format(class_num, teachers))
         classes [class_num-1] = class_num
         professors[class_num-1] = teachers
-    return professors, rooms
+    return professors, rooms, time_slots
 
 def parse_pref(file):
     dict = {}
@@ -53,7 +58,7 @@ def count_class_size(pref_dict):
     # content in n: (class id, popularity)
     n = sorted(sizes.items(), key=operator.itemgetter(1))
     n.reverse()
-    print(n)
+    # print(n)
     return n
 
 # classes is a list of clsses from count_class_size(), so it should be sorted by popularity already
@@ -67,20 +72,16 @@ def scheduling(classes, students, professors, times, rooms):
         class_id = pair[0]
         popularity = pair[1]
         room_id = 0
-        room_id, t, cap = find_valid_room(popularity, rooms)
-        Prof = []
-        for i in range (0,room_id-1,1):
-            if room_id > 1:
-                Prof.append(professors[Schedule[t][i]])
-        if professors[class_id] in Prof:
-            room_id, t, cap = find_valid_room(cap+1, rooms)
+        room_id, t, cap = find_valid_room(Schedule, popularity, rooms, professors, class_id)
         Schedule[t][room_id-1] = class_id
         Position[class_id-1] = (t,room_id-1)
+    print("----------Schedule-----------")
     print (Schedule)
+    print("-----------Position-----------")
     print(Position)
-    return Schedule
+    return Schedule, Position
 
-def find_valid_room(threshold, rooms):
+def find_valid_room(Schedule, threshold, rooms, professors, class_id):
     room_id = 0
     t = 0
     capacity = 0
@@ -88,32 +89,35 @@ def find_valid_room(threshold, rooms):
         if cap >= threshold:
             room_id = rid
             capacity = cap
-            t = empty_timeslot() 
+            t = empty_timeslot(Schedule, room_id, professors, class_id) 
             if not t == None:
                 break
     return room_id, t, capacity 
 
-def empty_timeslot(Schedule, room_id):
-    for row in Schedule:
+def empty_timeslot(Schedule, room_id, professors, class_id):
+    for row in range (len( Schedule)):
+        Prof = []
         if Schedule[row][room_id-1] == 0:
-            return row
+            for i in range (0,room_id-1,1):
+                Prof.append(professors[Schedule[row][i]-1])
+            if not professors[class_id-1] in Prof:
+                return row
     return None
     
-
 def sort_room_cap(Class_list):
     Class_list.sort(key = lambda x: x[1])
     Class_list.reverse()
-    print (Class_list)
+    #print (Class_list)
     return Class_list
     
 def test_result(S, Pref, Schedule, Position):
     count = 0
     total = 0
     for s in S:
-        total += len(Pref[c])
+        total += len(Pref[s])
         final_pick = [0] * (len(Schedule)+1)
         for c in Pref[s]:
-            t = Position[c+1][0]
+            t = Position[c-1][0]
             if final_pick[t] == 0:
                 final_pick[t] = c
                 count += 1
@@ -140,8 +144,11 @@ def edgeWeights(dict):
     # print(len(weight))
     return weight
 
-parse_classTimes("./demo_constraints.txt")
+professors, rooms, times = parse_classTimes("./demo_constraints.txt")
 dict = parse_pref("./demo_studentprefs.txt")
-count_class_size(parse_pref("./demo_studentprefs.txt"))
-sort_room_cap([(1,40),(2,50),(3,20),(4,30)])
+students = dict.keys()
+classes = count_class_size(parse_pref("./demo_studentprefs.txt"))
+rooms = sort_room_cap(rooms)
+schedule, position = scheduling(classes, students, professors, times, rooms)
+print(test_result(students, dict, schedule, position))
 #edgeWeights(dict)
